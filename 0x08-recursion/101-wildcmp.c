af6496eb2935853d19a94550f52826e2e0913076
MohammedHatim06 <mohatim28@gmail.com>
2023-09-28 15:05:19 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -62,6 +62,8 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
 		if (*s1 == *s2)
 			r += wildcmp(s1 + 1, s2 + 1);
 		r += inception(s1, s2);
